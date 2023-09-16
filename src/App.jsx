a84456b05fc982e1f6fac72c66b54b6d0fe3f998
Evangelene <siyinyap@gmail.com>
2023-09-16 23:09:35 +0800

    feat: style layout and design page

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Link, Route, Routes } from "react-router-dom";
+import Fridge from "./components/Fridge/Fridge";
+import "./App.css"
+
+function LandingPage() {
+  return (
+    <>
+      <h1>What's in your fridge?</h1>
+      <div className="fridge-container">
+        <Link to={`/fridge`} style={{ display: "flex", justifyContent: "center" }}>
+          <img className="closed-fridge" src="./assets/fridge-closed.png" />
+        </Link>
+        <Link to={`/fridge`} style={{ display: "flex", justifyContent: "center" }}>
+          <img className="open-fridge" src="./assets/fridge-open.png" />
+        </Link>
+      </div>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/fridge" element={<Fridge />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/components/Fridge/Fridge.jsx
@@ -0,0 +1,16 @@
+import "./Fridge.css"
+
+export default function Fridge () {
+    return (
+        <div className="container">
+        <h2>Track your Ingredients</h2>
+        <div className="ingredients-fridge"></div>
+        <table className="overlay-table"><tbody>
+            <tr style={{ padding: "200px" }}>
+                <td>
+            Hello</td>
+            </tr>
+            </tbody></table>
+        </div>
+    )
+};
