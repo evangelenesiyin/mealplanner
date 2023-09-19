7aee838f9dd261e89a4a5bee9aa261bfd866a76a
Evangelene <siyinyap@gmail.com>
2023-09-20 00:59:32 +0800

    docs: update title of search button in ingredient card

--- src/components/Fridge/IngredientCard.jsx
+++ src/components/Fridge/IngredientCard.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { Tooltip } from "@mui/material";
 import EditIngredient from './EditIngredient';
 import DeleteIngredient from './DeleteIngredient';
 
@@ -10,7 +11,7 @@ export default function IngredientCard({ fetchIngredients, id, name, type, purch
                     <p className="ingredient-name">{name}</p>
                 </span>
                 <span className="icon-container">
-                    <Link to={`/fridge/${id}`}><img className="icon" src="./assets/search.png" alt="Search recipes" /></Link>
+                    <Tooltip title="Search recipes"><Link to={`/fridge/${id}`}><img className="icon" src="./assets/search.png" alt="Search recipes" /></Link></Tooltip>
                     <EditIngredient />
                     <DeleteIngredient
                     id={id}
