d6f69b12381ad72244a1642e448f16c337782e2c
Evangelene <siyinyap@gmail.com>
2023-09-20 17:15:40 +0800

    delete file

--- src/components/Fridge/EditIngredient.jsx
+++
@@ -1,9 +0,0 @@
-import { Tooltip } from "@mui/material";
-
-export default function EditIngredient() {
-    return (
-        <Tooltip title="Edit">
-        <img className="icon" src="./assets/edit.png" alt="Edit" />
-        </Tooltip>
-    )
-}
