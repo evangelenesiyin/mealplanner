e891df68a12e315cebe13b1f345c362946c58282
Evangelene <siyinyap@gmail.com>
2024-02-08 23:39:37 +0800

    feat: add style/UI for deletion of ingredients
    now appears as a pop-up and asks for confirmation

--- src/components/Fridge/DeleteIngredient.jsx
+++ src/components/Fridge/DeleteIngredient.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import { Tooltip } from "@mui/material";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import LoadingButton from '@mui/lab/LoadingButton';
+import Button from '@mui/material/Button';
 
 export default function DeleteIngredient({ fetchIngredients, id }) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [loading, setLoading] = useState(false);
+
+    const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 300,
+    bgcolor: '#fff',
+    borderRadius: '3px',
+    p: 4,
+  };
 
     const handleRemove = async () => {
-        const decision = window.confirm("Are you sure?");
-
-    if (!decision) {
-    return;
-    }
-
+    setLoading(true);
     const AIRTABLE_API_KEY = 'patHEpY0OX1f5m692.3c173ace26d4a13ae350424ea67f610df9c6c2aa6486fd9008060dbe191ed051';
     const BASE_ID = 'appNo7BJCMjuy3aw5';
     const TABLE_NAME = 'Ingredients%20List';
@@ -22,11 +38,60 @@ export default function DeleteIngredient({ fetchIngredients, id }) {
     });
     await response.json();
     fetchIngredients();
+    setLoading(false);
+    handleClose();
     };
 
     return (
+      <>
         <Tooltip title="Delete">
-        <img onClick={handleRemove} className="icon" src="./assets/delete.png" alt="Delete" />
+        <img onClick={handleOpen} className="icon" src="./assets/delete.png" alt="Delete" />
         </Tooltip>
+
+        <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ fontSize: '18px', marginTop: '-10px', fontWeight: '300' }}>
+            Confirm to delete?
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2, textAlign: 'center' }}>
+            <LoadingButton
+              loading={loading}
+              onClick={handleRemove}
+              variant="contained"
+              sx={{
+                backgroundColor: 'rgb(250, 212, 102)',
+                color: '#000',
+                boxShadow: '3px 3px 5px rgba(0, 0, 0, 0.2)',
+                marginRight: '10px',
+                padding: '5px 15px',
+                textTransform: 'capitalize',
+                '&:hover': {
+                    backgroundColor: 'rgb(255, 230, 153)',
+                }
+              }}
+            >
+              Delete
+            </LoadingButton>
+            <Button
+            onClick={handleClose}
+            sx={{
+              backgroundColor: '#fff',
+              color: '#000',
+              boxShadow: '3px 3px 5px rgba(0, 0, 0, 0.2)',
+              padding: '5px 15px',
+              textTransform: 'capitalize',
+            }}
+            >
+              Cancel
+              </Button>
+          </Typography>
+        </Box>
+      </Modal>
+      </>
     )
 }
